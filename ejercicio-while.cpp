82a460291e7bea925fa2d8bcefc476d3c6b37e5d
Rodrix-ra34 <00167625@uca.edu.sv>
2025-05-22 12:43:33 -0600

    Add files via upload

---
+++ ejercicio-while.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+// libreria de entrada y salida
+
+int main()
+{
+  int numero=0; 
+  while(numero>=0){
+
+    int numero[10];
+    for(int i=0; i<10; i++){
+        cout<<"ingrese un numero: "<<i+1<<":"<<endl;
+        //se imprimen los valores
+        cin>>numero[i];
+        //se leen los valores
+    }
+  }
+  
+    return 0;
+}
